c694a79cc0885fc4431ab18df910b21b10545a7f
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-29 15:18:14 +0200

    sono partito dalla logica di veu js, ho creato la logica dei pulsanti next e pre, inserito al posto del p precedente e del titolo i {{}} e collegato le immagini da js a html facendo in modo che si vedano solo quando l'index === slidesIndex con v-show

--- vue-slider-markup-ex/js/script.js
+++ vue-slider-markup-ex/js/script.js
@@ -27,7 +27,16 @@ console.log(slides);
 const app = Vue.createApp({
     data(){
         return{
-
+            slides,
+            slidesIndex: 0
+        };
+    },
+    scorrimento: {
+        next(){
+            this.slidesIndex = (this.slidesIndex+1) % this.slides.length; // questo mi permette di ricominciare il giro delle immagini ogni volta che lo slidesIndex diviso con slides.lenght da resto 0.
+        },
+        pre(){
+            this.slidesIndex = (this.slidesIndex - 1 + this.slides.length) % this.slides.length; //questo mi permette di tornare al lenght corrispondente allo slideIndex ma se è sotto lo 0, quindi -1 riparte dall'ultimo della lista.
         }
     }
 });
